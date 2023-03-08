080c43dfd41ed4838aea1a040b12980751109766
Razial <uck15@bk.ru>
2023-03-08 12:19:41 +0300

    Add fuctions to button add and exit

--- Corse/GUI.py
+++ Corse/GUI.py
@@ -1,10 +1,40 @@
+import sys
+
 import functions
 import PySimpleGUI as sg
 
 label = sg.Text('Type in a to-do')
-input_box = sg.InputText(tooltip='Enter todo')
+input_box = sg.InputText(tooltip='Enter todo', key='todo')
 add_button = sg.Button('Add')
+exit_button = sg.Button('Exit')
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window('My To-Do App',
+                   layout=[[label],
+                           [input_box, add_button],
+                           [exit_button]],
+                   font=('Helvetica', 14))
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo']+'\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case 'Show':
+            pass
+
+        case 'Complete':
+            pass
+
+        case sg.WINDOW_CLOSED:
+            break
+
+        case 'Exit':
+            break
+
 window.close()
+
--- Corse/functions.py
+++ Corse/functions.py
@@ -1,11 +1,12 @@
-def get_todos(file_path='todos.txt'):
+FILEPATH = 'files/todos.txt'
+def get_todos(file_path=FILEPATH):
     """Read a file and return the list of to-do items"""
     with open(file_path, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_local, file_path='todos.txt'):
+def write_todos(todos_local, file_path=FILEPATH):
     """Write the to-do items list in the file"""
     with open(file_path, 'w') as file_local:
         file_local.writelines(todos_local)
